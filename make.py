0a1e456468f2fb3c48b87b1ab13a2c7a73a27d4c
Francesco Saccone <francesco@saccone.net>
2026-08-11 19:17:07 +0200

    Add ttf generation script

---
+++ make.py
@@ -0,0 +1,77 @@
+import fontforge
+import io
+import json
+import os
+import tempfile
+
+fontname = "Typ"
+charsdir = "chars"
+charwidth, charheight = 3, 3
+fontem = 300
+
+font = fontforge.font()
+
+font.familyname = fontname
+font.fontname = fontname
+font.fullname = fontname
+font.weight = "Regular"
+font.em = fontem
+font.ascent = fontem
+font.descent = 0
+font.version = "1.0"
+
+mappingfile = open("chars/mapping.json", "r")
+mapping = json.load(mappingfile)
+mappingfile.close()
+
+def gridtosvg(grid):
+	pixels = []
+
+	for y in range(charheight):
+		for x in range(charwidth):
+			if grid.replace("\n", "")[y * charwidth + x] == "#":
+				pixels.append([x, y])
+
+	svg = '<svg xmlns="http://www.w3.org/2000/svg" '
+	svg += f'width="{charwidth}" height="{charheight}" '
+	svg += f'viewBox="0 0 {charwidth} {charheight}" '
+	svg += 'preserveAspectRatio="xMidYMid meet" rendering="crispEdges">'
+
+	for p in pixels:
+		svg += f'<rect x="{p[0]}" y="{p[1]}" width="1" height="1" '
+		svg += 'fill="#000000" shape-rendering="crispEdges"></rect>'
+
+	if not pixels:
+		svg += '<rect x="0" y="0" width="0" height="0" fill="none">'
+		svg += '</rect>'
+
+	svg += '</svg>'
+
+	return svg
+
+for filename, chars in mapping.items():
+	filepath = os.path.join("chars", filename)
+
+	if not os.path.exists(filepath):
+		print(f"{filepath} does not exist.")
+		continue
+
+	file = open(filepath, "r")
+	svg = gridtosvg(file.read())
+	file.close()
+
+	tmp = tempfile.NamedTemporaryFile(delete=True, mode="w+", suffix=".svg")
+	tmp.write(svg)
+	tmp.seek(0)
+
+	for c in chars:
+		glyph = font.createMappedChar(ord(c))
+		glyph.importOutlines(tmp.name)
+		glyph.width = fontem
+		glyph.vwidth = fontem
+		glyph.left_side_bearing = round(fontem / charwidth / 2)
+		glyph.right_side_bearing = round(fontem / charwidth / 2)
+
+	tmp.close()
+
+font.generate(f"{fontname}.ttf")
