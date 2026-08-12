78e799c54e404a7e5a91670e133065e8b9c8ca9f
Francesco Saccone <francesco@saccone.net>
2026-08-12 13:03:30 +0200

    Define and use pxbetween

--- make.py
+++ make.py
@@ -7,6 +7,7 @@ import tempfile
 fontname = "Typ"
 charsdir = "chars"
 charwidth, charheight = 4, 5
+pxbetween = 1
 fontem = 400
 
 font = fontforge.font()
@@ -33,12 +34,12 @@ def gridtosvg(grid):
 				pixels.append([x, y])
 
 	svg = '<svg xmlns="http://www.w3.org/2000/svg" '
-	svg += f'width="{charwidth + 1}" height="{charheight}" '
-	svg += f'viewBox="0 0 {charwidth + 1} {charheight}" '
+	svg += f'width="{charwidth + pxbetween}" height="{charheight}" '
+	svg += f'viewBox="0 0 {charwidth + pxbetween} {charheight}" '
 	svg += 'preserveAspectRatio="xMidYMid meet" rendering="crispEdges">'
 
 	for p in pixels:
-		svg += f'<rect x="{p[0] + 0.5}" y="{p[1]}" width="1" '
+		svg += f'<rect x="{p[0] + pxbetween / 2}" y="{p[1]}" width="1" '
 		svg += 'height="1" fill="#000000" shape-rendering="crispEdges">'
 		svg += '</rect>'
 
